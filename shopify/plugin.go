5dbbc6f505bc222331d3cb22ebc3715125b395ad
Puskar Basu <puskar@turbot.com>
2023-04-25 15:51:34 +0530

    Add table shopify_customer. Closes #3

--- shopify/plugin.go
+++ shopify/plugin.go
@@ -19,7 +19,9 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			NewInstance: ConfigInstance,
 			Schema:      ConfigSchema,
 		},
-		TableMap: map[string]*plugin.Table{},
+		TableMap: map[string]*plugin.Table{
+			"shopify_customer": tableShopifyCustomer(ctx),
+		},
 	}
 	return p
 }
