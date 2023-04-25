9b8dea832f95fc164bff82303d0ca9df2704898e
Puskar Basu <puskar@turbot.com>
2023-04-25 18:20:56 +0530

    initial changes

--- shopify/plugin.go
+++ shopify/plugin.go
@@ -19,7 +19,9 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			NewInstance: ConfigInstance,
 			Schema:      ConfigSchema,
 		},
-		TableMap: map[string]*plugin.Table{},
+		TableMap: map[string]*plugin.Table{
+			"shopify_custom_collection": tableShopifyCustomCollection(ctx),
+		},
 	}
 	return p
 }
